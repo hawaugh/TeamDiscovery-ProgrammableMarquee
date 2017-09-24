e61856d0d4e27b3a8ecd1f65363a9cbb3c900397
Ahmad Omar <31226496+AhmadEOmar@users.noreply.github.com>
2017-09-24 17:34:28 -0400

    Update Dot.cs
    Changed how the getters and setters were set up. Followed what other recommend c# would be written.

--- Vision/Vision/Dot.cs
+++ Vision/Vision/Dot.cs
@@ -1,4 +1,4 @@
-﻿//Ahmad
+﻿//Edited on 9/24 Ahmad
 
 using System;
 using System.Collections.Generic;
@@ -8,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace Vision
 {
+  
     class Dot
+        {
+            private string _dotColor;
+            private static bool _setActive;
+
+
+            //Constructor
+            public Dot()
+            {
+                //This was just added to test that I would be able to find and test the Active getter and setter
+                this.Active = true;
+            }
+
+            //Most c# getter/setters are written in this fashion
+            public bool Active
+            {
+                get { return _setActive; }
+                set { _setActive = value; }
+            }
+
+            public string dotColor
+            {
+                get { return _dotColor; }
+                set { _dotColor = value; }
+            }
+        }
+}
+    
+    
+ // Original   
+ /* class Dot
     {
         private string dotColor;
         private bool active;
@@ -38,5 +69,4 @@ namespace Vision
         {
 
         }
-    }
-}
+    }*/
