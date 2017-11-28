1108e197b252e14dbe77210caa91377058c7d12b
jbmelvin <32202275+jbmelvin@users.noreply.github.com>
2017-11-28 14:04:38 -0500

    DotTest started
    first test in DotTest complete

---
+++ DotTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Vision.Tests
+{
+    [TestClass()]
+    public class DotTests
+    { 
+        [TestMethod()]
+        public void DotTest()
+        {
+            Color _ForeColor;
+            _ForeColor = Color.DarkGray;
+            Assert.AreEqual(_ForeColor, Color.DarkGray);
+        }
+
+        [TestMethod()]
+        public void randColorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
